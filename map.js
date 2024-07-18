a43f7ce7238ec478322a8142cc57ec7c58deb0f1
KarthikAyikkathil100 <110910468+KarthikAyikkathil100@users.noreply.github.com>
2024-07-18 18:13:54 +0530

    Add files via upload

--- map.js
+++ map.js
@@ -1,12 +1,26 @@
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
     try {
         console.log('Map function');
         console.log(event)
-        const obj = {
-            message: 'Map success'
-        }
-        callback(null, obj)
+        const res = await mockHeavyOperation(event)
+        callback(null, res)
     } catch (e) {
+        console.log('Error in map => ', e)
         throw e
     }
+}
+
+async function mockHeavyOperation(obj) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            setTimeout(() => {
+                console.log(obj)
+                resolve({
+                    id: obj.id * 100
+                })
+            }, 3000);
+        } catch (e) {
+            reject(e)
+        }
+    })
 }
--- step2.js
+++ step2.js
@@ -3,8 +3,11 @@ exports.handler = (event, context, callback) => {
         // throw new Error('Test step 2 failure')
         console.log('step 2 logging');
         console.log(event)
+        const count = 10;
+        const data = []
+        for (let i = 0; i < count; i++) data.push({id: i+1})
         const obj = {
-            "data": [1,2,3,4]
+            data,
         }
         callback(null, obj)
     } catch (e) {
