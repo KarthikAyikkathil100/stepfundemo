26517e26dfb720731c3b47016b22c05c9ea18fea
KarthikAyikkathil100 <110910468+KarthikAyikkathil100@users.noreply.github.com>
2024-07-24 18:03:13 +0530

    Add Lambda docker support

--- images/index.js
+++ images/index.js
@@ -1,10 +1,20 @@
+const bcrypt = require('bcrypt')
+
 exports.handler = (event, context, callback) => {
     try {
+        console.log('Inside it')
         console.log(event)
-        const obj = {
-            "message": "This is from docker function"
-        }
-        callback(null, obj)
+        bcrypt.hash('Harry Potter', 10)
+            .then((hash) => {
+                const obj = {
+                    "message": "This is from docker function",
+                    "data": hash
+                }
+                callback(null, obj)
+            })
+            .catch((e) => {
+                throw e
+            })
     } catch (e) {
         callback(null, {
             error: true,
